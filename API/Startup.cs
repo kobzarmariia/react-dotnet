e244d5ee9f942dd4b8cb2541983444b3c5e0fe0d
maria.kobzar <maria.kobzar@formpipe.com>
2021-04-04 20:11:51 +0300

    Added AutoMapper

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,14 +2,13 @@ using Application.Activities;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Persistence;
+using Application.Core;
 
 namespace API
 {
@@ -46,6 +45,8 @@ namespace API
             });
 
             services.AddMediatR(typeof(List.Handler).Assembly);
+
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
