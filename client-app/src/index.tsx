06c874a49c460d91f6b8b607fea1b5dd4197b71f
maria.kobzar <maria.kobzar@formpipe.com>
2021-04-05 20:39:15 +0300

    Styling UI

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
+import 'react-calendar/dist/Calendar.css';
 import App from './app/layout/App';
 import {StoreContext, store} from "./app/stores/store";
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <StoreContext.Provider value={store}>
-    <App />
-  </StoreContext.Provider>,
-  document.getElementById('root')
+    <StoreContext.Provider value={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StoreContext.Provider>,
+    document.getElementById('root')
 );
