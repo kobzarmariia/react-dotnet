b99a564e1eb91b37248b5f62d3e754810b35bf64
maria.kobzar <maria.kobzar@formpipe.com>
2021-04-05 17:51:11 +0300

    Added MobX

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
+import {StoreContext, store} from "./app/stores/store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StoreContext.Provider value={store}>
     <App />
-  </React.StrictMode>,
+  </StoreContext.Provider>,
   document.getElementById('root')
 );
